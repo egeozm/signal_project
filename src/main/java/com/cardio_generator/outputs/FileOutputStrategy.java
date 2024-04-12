f6db9732ccb69c21917597338faa7f4d1ac4fe64
egeozm <egeozm@gmail.com>
2024-04-12 20:32:20 +0200

    Both files, FileOutputStrategy.java and AlertGenerator.java, have been corrected.

---
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -0,0 +1,45 @@
+package com.cardio_generator.outputs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.ConcurrentHashMap;
+
+// Changed the class name form "fileOutStrategy" to "FileOutputStrategy". Class name starts with uppercase.
+public class FileOutputStrategy implements OutputStrategy {
+
+    // Changed "BaseDirectory" to "baseDirectory". Variable start with lowercase.
+    private String baseDirectory;
+
+    // Changed "file_map" to "FILE_MAP". Constant naming conventions which use uppercase letters with underscores.
+    public final ConcurrentHashMap<String, String> FILE_MAP = new ConcurrentHashMap<>();
+
+    public FileOutputStrategy(String baseDirectory) {
+
+        this.baseDirectory = baseDirectory;
+    }
+
+    @Override
+    public void output(int patientId, long timestamp, String label, String data) {
+        try {
+            // Create the directory
+            Files.createDirectories(Paths.get(baseDirectory));
+        } catch (IOException e) {
+            System.err.println("Error creating base directory: " + e.getMessage());
+            return;
+        }
+        // Set the FilePath variable
+        // Changed "FilePath" to "filePath". Variable start with lowercase.
+        String filePath = FILE_MAP.computeIfAbsent(label, k -> Paths.get(baseDirectory, label + ".txt").toString());
+
+        // Write the data to the file
+        try (PrintWriter out = new PrintWriter(
+                Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
+            out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
+        } catch (Exception e) {
+            System.err.println("Error writing to file " + filePath + ": " + e.getMessage());
+        }
+    }
+}
